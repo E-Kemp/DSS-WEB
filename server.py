d100541f9a5a860b5e847c855c9a9b4316f1b7dc
Elliot Kemp <mail@elliotjordankemp.com>
2020-02-12 13:06:11 +0000

    Testing implementation methods with Jinja2 and Flask

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
+
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return 'Hello, World!'
+    data = {"title":"Test",
+        "username":"wibble",
+        "content":"hello world!"}
+    post2 = {"title":"Test",
+        "username":"wibble",
+        "content":"hello world!"}
+        
+    posts = [data, post2]    
+    return render_template('home.html', posts = posts);
     
 if __name__ == "__main__":
-    app.run();
+    app.run(debug=True);
