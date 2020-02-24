d16e688b9887bdeb2bb0cb38171019cd944a462e
Elliot Kemp <mail@elliotjordankemp.com>
2020-02-24 14:44:19 +0000

    Added pages, changed colour scheme, added user detection in Jinja

--- server.py
+++ server.py
@@ -1,20 +1,67 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, redirect, render_template, request, flash
 app = Flask(__name__)
-
 
 @app.route('/')
 @app.route('/home')
-def hello_world():
-    data = {"title":"Test",
+def home():
+    user = None
+    data = {
+        "title":"Test",
         "username":"wibble",
         "content":"hello world!"}
-    post2 = {"title":"Test",
+    post2 = {
+        "title":"Test",
         "username":"wibble",
         "content":"hello world!"}
-        
     posts = [data, post2]    
-    return render_template('home.html', posts = posts);
+    return render_template('home.html', user = user, posts = posts)
     
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    user = None
+    if request.method == 'POST':
+        search = request.form['search']
+        return render_template('search.html', user = user, search = search)
+    return redirect('/home')
+
+    
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    user = None
+    error = None
+    if request.method == 'POST':
+        error = 'There\'s no database API yet! Try again later'
+    return render_template('login.html', user = user, error = error)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    user = None
+    error = None
+    if request.method == 'POST':
+        username = request.form['usernameInput']
+        email = request.form['emailInput']
+        password = request.form['passwordInput'] #pass directly into API later?
+        verify = request.form['verifyPasswordInput']
+        if(password != verify):
+            error = 'The passwords didn\'t match!'
+        else:
+            error = 'There\'s no database API yet! Try again later'
+    return render_template('register.html', user = user, error = error)
+
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    user = None
+    #error = None
+    if request.method == 'POST':
+        title = request.form['titleInput']
+        post = request.form['postInput']
+    return render_template('post.html', user = user)#, error = error)
+
+
 if __name__ == "__main__":
-    app.run(debug=True);
+    app.run(debug=True)
