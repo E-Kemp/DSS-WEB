7479d4468f1488428a7608e6065cfcb73acc68d3
Niklas Henderson <niklas.henderson@btinternet.com>
2020-03-08 03:38:44 +0000

    delete user

--- static/js/page-handling/settings-handler.js
+++ static/js/page-handling/settings-handler.js
@@ -21,13 +21,22 @@ $(document).ready(function() {
         });
     })
     
-    $('#delete-button').click(function() {
+    $('#delete-button').click(function(e) {
+		e.preventDefault();
+		
         var postUrl = 'http://127.0.0.1:5000/account/deleteAccount';
         var postType = 'post';
         var formID = '#delete-form';
 
         formHandler(postType, postUrl, formID, function(response) {
-            window.location.replace('http://127.0.0.1:5432/logout')
+			if (response["code"] == "success"){
+				 window.location.replace('http://127.0.0.1:5432')
+			}else{
+				clearMessages();
+				message(response["code"], response["reason"]);
+				
+			}
+           
         });
     });
 })
